4ffc3326148b25a711411b45f73102ff36e755b5
seika <43219386+syfgkjasdkn@users.noreply.github.com>
2019-03-31 10:25:53 +0000

    fix dev config typo

--- config/dev.exs
+++ config/dev.exs
@@ -6,7 +6,7 @@ use Mix.Config
 # The watchers configuration can be used to run external
 # watchers to your application. For example, we use it
 # with webpack to recompile .js and .css sources.
-config :janus_ws_example, JanusWsExampleWeb.Endpoint,
+config :janus_ws_example, Web.Endpoint,
   http: [port: 4000],
   debug_errors: true,
   code_reloader: true,
